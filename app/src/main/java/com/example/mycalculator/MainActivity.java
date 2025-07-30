e01eab3b667b7105d876ee7dfbfd2374f976c167
RohByungWan <byung6419@naver.com>
2025-07-30 17:49:30 +0900

    refactor: Simplify MainActivity.java logic refactor: Tag activity_main_xml

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -26,17 +26,16 @@ public class MainActivity extends AppCompatActivity {
 
     }
     //numButtonClick 메서드로 xml 에 있는 Button을 한번에 등록 하여 코드
-    //가독성과 생산성 증가했음
+    //가독성과 생산성 증가
+    // xml에 Tag 사용하여 바로 view에서 .getTag 로 불러와 가독성 확보
     public void numButtonClick(View view) {
-        Button button = findViewById(view.getId());
-        String getButtonText = button.getText().toString();
         if (isFirstInput) {
-            activityMainBinding.resultTextView.setText(getButtonText);
-            activityMainBinding.formulaTextView.setText(getButtonText);
+            activityMainBinding.resultTextView.setText(view.getTag().toString());
+            activityMainBinding.formulaTextView.setText(view.getTag().toString());
             isFirstInput = false;
         }else  {
-            activityMainBinding.resultTextView.append(getButtonText);
-            activityMainBinding.formulaTextView.append(getButtonText);
+            activityMainBinding.resultTextView.append(view.getTag().toString());
+            activityMainBinding.formulaTextView.append(view.getTag().toString());
         }
     }
 
