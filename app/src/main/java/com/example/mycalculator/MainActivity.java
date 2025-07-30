60bfc7e43d9bf3189dff522cd698736566a47ab2
RohByungWan <byung6419@naver.com>
2025-07-30 17:32:36 +0900

    refactor: Simplify MainActivity.java logic

--- app/src/main/java/com/example/mycalculator/MainActivity.java
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -25,7 +25,8 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-
+    //numButtonClick 메서드로 xml 에 있는 Button을 한번에 등록 하여 코드
+    //가독성과 생산성 증가
     public void numButtonClick(View view) {
         Button button = findViewById(view.getId());
         String getButtonText = button.getText().toString();
@@ -38,46 +39,6 @@ public class MainActivity extends AppCompatActivity {
             activityMainBinding.formulaTextView.append(getButtonText);
         }
     }
-
-
-
-    //계산기에서 0~9번 까지의 버튼 이벤트 실행
-    //isFirstInput이 위에 boolean으로 true가 선언 되어있어 만약에 true이면 setText실행 후
-    //isFirstInput을 false로 바꿔 else로 넘어가게 한 후 append 실행
-    //이렇게 되면 계산기 앱에서 1을 연속적으로 누를 수 있다.
-    public void num1Click(View view) {
-        if (isFirstInput) {
-            activityMainBinding.resultTextView.setText("1"); // setText는 기존에 있는 내용을 지우고
-            activityMainBinding.formulaTextView.setText("1");// 새로운 문자열을 셋팅해주는 메서드.
-            isFirstInput = false;
-        }else {
-            activityMainBinding.resultTextView.append("1"); // append는 기존 내용을 유지한 채
-            activityMainBinding.formulaTextView.append("1");// 뒤에 문자열을 붙혀주는 메서드
-        }
-    }
-
-    public void num2Click(View view) {
-        if (isFirstInput) {
-            activityMainBinding.resultTextView.setText("2"); // setText는 기존에 있는 내용을 지우고
-            activityMainBinding.formulaTextView.setText("2");// 새로운 문자열을 셋팅해주는 메서드.
-            isFirstInput = false;
-        }else {
-            activityMainBinding.resultTextView.append("2"); // append는 기존 내용을 유지한 채
-            activityMainBinding.formulaTextView.append("2");// 뒤에 문자열을 붙혀주는 메서드
-        }
-    }
-
-    public void num3Click(View view) {
-        if (isFirstInput) {
-            activityMainBinding.resultTextView.setText("3");
-            activityMainBinding.formulaTextView.setText("3");
-            isFirstInput = false;
-        }else {
-            activityMainBinding.resultTextView.append("3");
-            activityMainBinding.formulaTextView.append("3");
-        }
-    }
-
 
 
 
